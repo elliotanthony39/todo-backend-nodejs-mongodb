2effe5ae7153f768b9598dbaac1bd77f5fe6ea8e
elliotanthony39 <elliotant_tony@hotmail.com>
2023-01-09 17:13:15 -0500

    cors added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes/index.routes");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 //Middleware
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
